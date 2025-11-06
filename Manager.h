424d0771e52b50e012c51e5436bc391dec181a74
aa <03xodbs@gmail.com>
2025-11-06 13:24:12 +0900

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -21,14 +21,16 @@ public:
 	}
 
 	~Manager()
-	{ // destructor
+	{
 		if (bptree)
 		{
 			delete bptree;
+			bptree = nullptr;
 		}
 		if (stree)
 		{
 			delete stree;
+			stree = nullptr;
 		}
 		if (flog.is_open())
 		{
